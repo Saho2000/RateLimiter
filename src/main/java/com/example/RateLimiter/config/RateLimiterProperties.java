401c6f789b22d9ae1f8f077a1c9f1352059630f2
Sahosrabdi <sahosrabdidas.2016@gmail.com>
2026-02-08 10:33:55 +0530

    Added rate limiter logic and updated configuration

---
+++ src/main/java/com/example/RateLimiter/config/RateLimiterProperties.java
@@ -0,0 +1,16 @@
+package com.example.RateLimiter.config;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "rate-limiter")
+public class RateLimiterProperties {
+    private long capacity=10;
+    private long refillRate=5;
+    private String apiServerUrl="https://localhost:8080";
+    private int timeout=5000;
+}
